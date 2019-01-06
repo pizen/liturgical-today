6a65bf69e5240e49ebc0b5df86fa3192fc672aa2
Ian Turner <imturner@gmail.com>
2019-01-05 21:29:09 -0500

    Add /reformed/<date> API endpoint in GCP App Engine

---
+++ main.py
@@ -0,0 +1,24 @@
+from datetime import date
+from flask import abort, Flask, Response
+import json
+from pyliturgical import calendar
+
+app = Flask(__name__)
+
+
+@app.route('/reformed/<date_str>')
+def reformed(date_str):
+    try:
+        d = date.fromisoformat(date_str)
+    except Exception:
+        abort(400)
+
+    return Response(
+        json.dumps(calendar.lookup(d)),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
